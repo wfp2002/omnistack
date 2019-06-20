5a3fde8199ecfbf5c377b4e57f3f5c428a69ff24
wfp2002 <wfp2002@msn.com>
2019-06-20 17:14:47 -0300

    Primeiro Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Routes from './routes';
+//teste
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
